b2c271b94f8a495fc727ed64b09346ec94e4f5e3
thatshowitworks <62587456+thatshowitworks@users.noreply.github.com>
2020-10-22 12:25:44 +0530

    Create adduser.py

---
+++ plugins/adduser.py
@@ -0,0 +1,39 @@
+from pyrogram import filters
+from bot import app
+
+@app.on_message(filters.command("adduser"))
+async def add(client, update):
+	     if update.from_user['id'] not in AUTH_USERS:
+		       return
+	     msg = update.text
+	     reply_message = update.reply_to_message
+	     if reply_message:
+	         add_user(reply_message.from_user["id"], "k")
+	         await update.reply("User approved to use bot")
+	     else:
+	       try:
+	         user_id = int(msg.split(" ", 1)[1], "k")
+	         add_user(user_id)
+	         await update.reply("User approved to use bot")
+	       except:
+	         await message.reply("Format: `/adduser user id`")
+	         return
+
+@app.on_message(filters.command("rmuser"))
+async def remove(client, update):
+	     if update.from_user['id'] not in AUTH_USERS:
+		       return
+	     msg = update.text
+	     reply_message = update.reply_to_message
+	     if reply_message:
+	         remove_user(reply_message.from_user["id"])
+	         await update.reply("User unapproved to use bot")
+	     else:
+	       try:
+	         user_id = int(msg.split(" ", 1)[1])
+	         add_user(user_id)
+	         await update.reply("User unapproved to use bot")
+	       except:
+	         await message.reply("Format: `/rmuser user id`")
+	         return
+	     	     
