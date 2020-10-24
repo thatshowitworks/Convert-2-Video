7bf5a2b8fa663279a76268597f25fdb9f67dc687
thatshowitworks <62587456+thatshowitworks@users.noreply.github.com>
2020-10-24 15:03:07 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -29,7 +29,7 @@ from helper_funcs.chat_base import TRChatBase
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["setthumb"]))
-async def generate_custom_thumbnail(bot, update):
+async def save_photo(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
@@ -40,49 +40,28 @@ async def generate_custom_thumbnail(bot, update):
     app = user_approved(update)
     if not app and update.from_user.id not in Config.AUTH_USERS:
          return
-    TRChatBase(update.from_user.id, update.text, "generatecustomthumbnail")
-    if update.reply_to_message is not None:
-        reply_message = update.reply_to_message
-        if reply_message.media_group_id is not None:
-            download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(reply_message.media_group_id) + "/"
-            save_final_image = download_location + str(round(time.time())) + ".jpg"
-            list_im = os.listdir(download_location)
-            if len(list_im) == 2:
-                imgs = [ Image.open(download_location + i) for i in list_im ]
-                inm_aesph = sorted([(numpy.sum(i.size), i.size) for i in imgs])
-                min_shape = inm_aesph[1][1]
-                imgs_comb = numpy.hstack(numpy.asarray(i.resize(min_shape)) for i in imgs)
-                imgs_comb = Image.fromarray(imgs_comb)
-                # combine: https://stackoverflow.com/a/30228789/4723940
-                imgs_comb.save(save_final_image)
-                # send
-                await bot.send_photo(
-                    chat_id=update.chat.id,
-                    photo=save_final_image,
-                    caption=Translation.CUSTOM_CAPTION_UL_FILE,
-                    reply_to_message_id=update.message_id
-                )
-            else:
-                await bot.send_message(
-                    chat_id=update.chat.id,
-                    text=Translation.ERR_ONLY_TWO_MEDIA_IN_ALBUM,
-                    reply_to_message_id=update.message_id
-                )
-            try:
-                [os.remove(download_location + i) for i in list_im ]
-                os.remove(download_location)
-            except:
-                pass
-        else:
-            await bot.send_message(
-                chat_id=update.chat.id,
-                text="Reply to a media",
-                reply_to_message_id=update.message_id
-            )
+    TRChatBase(update.from_user.id, update.text, "save_photo")
+    r = update.reply_to_message
+    if r.media_group_id is not None:
+        # album is sent
+        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(update.media_group_id) + "/"
+        # create download directory, if not exist
+        if not os.path.isdir(download_location):
+            os.makedirs(download_location)
+        await bot.download_media(
+            message=r,
+            file_name=download_location
+        )
     else:
+        # received single photo
+        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+        await bot.download_media(
+            message=r,
+            file_name=download_location
+        )
         await bot.send_message(
             chat_id=update.chat.id,
-            text="Reply to a media",
+            text=Translation.SAVED_CUSTOM_THUMB_NAIL,
             reply_to_message_id=update.message_id
         )
 
